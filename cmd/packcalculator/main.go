e8bf261fd463893ca15f595c737c1cd88388c156
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2024-01-23 22:03:48 +0600

    CORS settings have been added.

--- cmd/packcalculator/main.go
+++ cmd/packcalculator/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 
 	"rpg/internal/packcalculator/handlers"
 )
 
 func main() {
-	// Handle requests to the '/calculate' endpoint using the CalculateHandler function.
-	http.HandleFunc("/calculate", handlers.CalculateHandler)
+	// Create a new router from the "gorilla/mux" package.
+	router := mux.NewRouter()
 
-	// Start the HTTP server on port 8080.
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Handle requests to the '/calculate' endpoint using the CalculateHandler function.
+	router.HandleFunc("/calculate", handlers.CalculateHandler).Methods("POST")
+
+	// Use the cors.Default() function to enable CORS with default options.
+	corsHandler := cors.Default().Handler(router)
+
+	// Get the port from the environment variable or use a default value (8080).
+	port := os.Getenv("RPG_BACKEND_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Log the information about the server starting.
+	log.Printf("Server starting on port %s...\n", port)
+
+	// Start the HTTP server on the specified port with CORS handling.
+	if err := http.ListenAndServe(":"+port, corsHandler); err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -13,8 +13,10 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/cors v1.10.1 // indirect
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
